da6679a4a23ee3ed527dbfd3847a828f9472e466
clement.t <clement.tolois@gmail.com>
2024-04-16 23:35:25 +0200

    chore: test utils

---
+++ src/app/user/domain/dto/request/create-user-api-request.dto.ts
@@ -0,0 +1,6 @@
+export interface ICreateUserBodyRequestDto {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+}
+
--- src/app/user/domain/dto/request/create.ts
+++
@@ -1,6 +0,0 @@
-export interface ICreateUserRequestDto {
-  name: string;
-  email: string;
-  password: string;
-}
-
